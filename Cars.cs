ca16c0028a95803afbe556e264e8112892584071
AndrewTateIsTheGOAT <josip.saravanja@elev.ga.lbs.se>
2024-09-27 11:34:10 +0200

    Add project files.

---
+++ Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Dahl
+{
+    internal class Cars
+    {
+        protected string _carType;
+        protected string _shiftGear;
+        protected int _mileage;
+        protected string _fuel;
+        public int _userAge;
+    }
+}
---
+++ CheckForFittingCars.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Dahl
+{
+    internal class CheckForFittingCars
+    {
+        public List<Cars> CarList = new List<Cars>
+        {
+            
+        };
+            public int _basePrice;
+    }
+}
