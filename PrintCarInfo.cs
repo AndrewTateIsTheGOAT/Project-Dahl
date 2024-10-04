5c15792d2bb0e390364c1827240fee117764bdba
AndrewTateIsTheGOAT <josip.saravanja@elev.ga.lbs.se>
2024-10-04 11:44:20 +0200

    04/10

--- PrintCarInfo.cs
+++ PrintCarInfo.cs
@@ -10,7 +10,39 @@ namespace Project_Dahl
     {
         protected string _userCarChoice;
         protected double UserCarChoicePrice;
+        private CheckForFittingCars Fitting = new CheckForFittingCars();
 
+        private string AskUserForCarChoice()
+        {
+            if ()
+            {
+                Console.WriteLine("Vilken DragkroksKapacitet vill du ha?");
+                string UserAnswer = Console.ReadLine();
+
+
+            }
+            else if ()
+            {
+                Console.WriteLine("Hur många hästkrafter vill du ha?");
+                string UserAnswer = Console.ReadLine();
+            }
+            else if ()
+            {
+                Console.WriteLine("¨Vilken komfort nivå vill du hà?");
+                string UserAnswer = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Choose one of these cars: ");
+            for (int i = 0; i < Fitting.CarList.Count; i++)
+            {
+                Console.WriteLine(Fitting.CarsThatFit("Sport", "Manual", 10000, "Diesel"));
+            }
+
+
+
+
+            return "ERROR";
         public string CarChoice
         {
             get { return _userCarChoice; }
